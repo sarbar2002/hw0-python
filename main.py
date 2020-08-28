8c230b7b5e333110755d29bfbe3141e8aa307c2e
sarbar2002 <sxs6666@psu.edu>
2020-08-28 22:43:04 +0000

    period

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 temp = input("Enter temperature in celsius: ")
 temp = float(temp)
-print(str(temp) + "° in Celsius is equivalent to " + str((temp * 9/5) + 32) + "° Fahrenheit")
+print(str(temp) + "° in Celsius is equivalent to " + str((temp * 9/5) + 32) + "° Fahrenheit.")
 
 
 
